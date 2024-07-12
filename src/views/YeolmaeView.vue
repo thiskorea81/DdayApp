6bcd4056d2b4300d8ba43016a3788cecc0e764fa
thiskorea81 <161725208+cjcbkfem@users.noreply.github.com>
2024-07-12 14:19:20 +0900

    열매 뷰 수정

--- src/views/YeolmaeView.vue
+++ src/views/YeolmaeView.vue
@@ -1,73 +1,82 @@
 <template>
-    <div class="result-view">
-      <h1>{{ ddayText }}</h1>
-      <p>열매</p>
-    </div>
-  </template>
-  
-  <script>
-  import moment from 'moment';
-  
-  export default {
-    data() {
-      return {
-        birthday: '2024-07-11',
-        dday: null
-      };
-    },
-    computed: {
-      ddayText() {
-        return `D${this.dday >= 0 ? '+' : ''}${this.dday+1}`;
-      }
-    },
-    mounted() {
-      this.calculateDday();
-      setInterval(this.calculateDday, 86400000); // 24시간마다 업데이트
-    },
-    methods: {
-      calculateDday() {
-        const today = moment();
-        const birthDate = moment(this.birthday);
-        this.dday = today.diff(birthDate, 'days');
-      }
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .result-view {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    background: linear-gradient(180deg, #000000, #434343);
-    color: white;
-    text-align: center;
-  }
-  
-  .result-view h1 {
-    font-size: 15em;
-    margin: 0;
-    color: #6a00ff;
-    background: -webkit-linear-gradient(#00f260, #0575e6);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-  }
-  
-  .result-view p {
-    font-size: 3em;
-    margin: 10px 0 0;
-  }
+  <div class="result-view">
+    <h1>{{ ddayText }}</h1>
+    <p>열매</p>
+  </div>
+</template>
 
-  @media (max-width: 600px) {
+<script>
+import moment from 'moment';
+
+export default {
+  data() {
+    return {
+      birthday: '2024-07-11',
+      dday: null
+    };
+  },
+  computed: {
+    ddayText() {
+      return `D${this.dday >= 0 ? '+' : ''}${this.dday + 1}`;
+    }
+  },
+  mounted() {
+    this.calculateDday();
+    setInterval(this.calculateDday, 86400000); // 24시간마다 업데이트
+  },
+  methods: {
+    calculateDday() {
+      const today = moment();
+      const birthDate = moment(this.birthday);
+      this.dday = today.diff(birthDate, 'days');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.result-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: linear-gradient(180deg, #000000, #434343);
+  color: white;
+  text-align: center;
+}
+
+.result-view h1 {
+  font-size: 15em;
+  margin: 0;
+  color: #6a00ff;
+  background: -webkit-linear-gradient(#00f260, #0575e6);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.result-view p {
+  font-size: 2em;
+  margin: 10px 0 0;
+}
+
+@media (max-width: 768px) {
   .result-view h1 {
-    font-size: 8em;
+    font-size: 10em;
   }
   
   .result-view p {
-    font-size: 3em;
+    font-size: 1.5em;
   }
 }
-  </style>
+
+@media (max-width: 480px) {
+  .result-view h1 {
+    font-size: 6em;
+  }
   
+  .result-view p {
+    font-size: 1em;
+  }
+}
+</style>
